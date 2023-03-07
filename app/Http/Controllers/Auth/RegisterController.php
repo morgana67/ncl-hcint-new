40711715cfba5f0a913b6275e016dc223af00980
morgana67 <126517364+morgana67@users.noreply.github.com>
2023-03-07 10:32:05 -0500

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -163,6 +163,7 @@ class RegisterController extends Controller
                     'token' => $token
                 ];
                 $body = str_replace("{{LINK_VERIFY}}", route('verifyAccount').'?'.http_build_query($paramGet), $body);
+                dd('sending confirmation email');
                 event(new SendMailProcessed($request->email,$mailConfig->subject,$body));
             }
             if(isset($request->is_doctor_register)) {
