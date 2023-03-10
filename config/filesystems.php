acead9f707822cc1344ff90fd7c0eeac0580dfc0
morgana67 <126517364+morgana67@users.noreply.github.com>
2023-03-10 12:42:55 -0500

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -77,9 +77,13 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+//     'links' => [
+//         public_path('storage') => storage_path('app/public'),
+//     ],
+    
+        'links' => [
+            base_path('public_html/storage') => storage_path('app/public'),
+        ],
 
 
 ];
