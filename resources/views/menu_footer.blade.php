5c28f31132eb9931e7b1def07bd3383b6c26df7b
Logan Mathews <logan.mathews@healthcareintegrations.com>
2024-01-23 15:45:26 -0600

    update google maps

--- resources/views/menu_footer.blade.php
+++ resources/views/menu_footer.blade.php
@@ -1,7 +1,7 @@
 @foreach($items as $menu_item)
     @if($menu_item->title != 'Doctor Registration')
         <div class="ftr__box ftr__links col-sm-4 clrlist listview">
-            <h4>{{ $menu_item->title }}</h4>
+            <h4>{{ $menu_item->title }}1</h4>
             @if(!$menu_item->children->isEmpty())
                 <ul>
                     @foreach($menu_item->children as $children)
