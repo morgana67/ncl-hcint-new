c0d0d202db85bf0efb43007f9f76037b95f79fab
Logan Mathews <logan.mathews@healthcareintegrations.com>
2024-01-23 15:49:21 -0600

    update Doctor Reg

--- resources/views/menu_footer.blade.php
+++ resources/views/menu_footer.blade.php
@@ -1,14 +1,10 @@
 @foreach($items as $menu_item)
-    @if()
-        @endif
     <div class="ftr__box ftr__links col-sm-4 clrlist listview">
         <h4>{{ $menu_item->title }}</h4>
         @if(!$menu_item->children->isEmpty())
             <ul>
                 @foreach($menu_item->children as $children)
-                    @if($children->title != 'Doctor Registration')
-                        <li><a href="{{ $children->link() }}">{{ $children->title }}</a></li>
-                    @endif
+                    <li><a href="{{ $children->link() }}">{{ $children->title }}</a></li>
                 @endforeach
             </ul>
         @endif
