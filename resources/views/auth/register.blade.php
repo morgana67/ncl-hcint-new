a46998cca964d2b06f74be771547abc5d5b8b03e
Jamie Dunn <jdunn82k@gmail.com>
2025-04-08 20:59:16 -0400

    hotfix captcha issues

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -219,6 +219,7 @@
                     <div class="g-recaptcha"
                          data-callback="enableBtn"
                          data-sitekey="6LcRB8goAAAAAALXWDBXS2PYaCZpH8TnYgRHiHcJ"></div>
+                    
                     <div class="form-group col-sm-6 text-right">
                         <button type="submit" id="submit_signup" class="btn btn-flat btn-primary " disabled>SIGNUP</button>
                     </div>
